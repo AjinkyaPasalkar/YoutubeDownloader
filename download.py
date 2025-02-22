5714442797b1abce292bc0da4e5c09b373675512
AjinkyaPasalkar <ajinkyaphy@gmail.com>
2025-02-22 22:04:46 +0530

    Use pytubefix instead of pytube

--- download.py
+++ download.py
@@ -1,16 +1,13 @@
 import os
-from pytube import YouTube
+from pytubefix import YouTube
 
-url = 'https://www.youtube.com/watch?v=x5oXSGhK7EY'
-file_name = 'Test_File'
-
-mp4 = file_name + '.mp4'
+url = input('Enter URL: ')
+filename = input('Enter filename: ')
 
 yt = YouTube(url)
-
 print(yt.streams.filter(only_audio=True))
-
 sel = input('Select stream itag:')
 
+
 stream = yt.streams.get_by_itag(int(sel))
-stream.download(filename=file_name)
+stream.download(filename=filename+'.mp3')
